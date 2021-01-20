9d1aa0d60693c75257e0ed1a30414e29af068b46
Dmitry Yagodin <dmitry.yagodin@gmail.com>
2021-01-20 14:53:27 +0200

    Added Computer Play

--- wordGame.js
+++ wordGame.js
@@ -1,27 +1,28 @@
 // Wordgame reworked from the python code ps4a.py and ps4b.py
-import wordList from "./wordList.js";
-const WORDLIST = wordList.split(",");
-console.log(WORDLIST.length);
+
+import WORDS from "./tests/words.js";
 const userInputButton = document.querySelector("#user-input-button");
+const COMPPLAY = document.querySelector('#computer');
 const userInput = document.querySelector("#user-input");
 const DIALOGUE = document.querySelector("#dialogue-div");
 const DISPLAYHAND = document.querySelector("#display-hand");
 const VOWELS = 'aeiou';
-const CONSONANTS = 'bcdfghjklmnpqrstvwxyz'
-const HAND_SIZE = 10
-const SCRABBLE_LETTER_VALUES = {
+const CONSONANTS = 'bcdfghjklmnpqrstvwxyz';
+const HANDSIZE = 8;
+const LETTER_VALUES = {
   'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
    'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
     's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
 }
+// const LETTERS = Object.keys(HAND).sort();
 
-function getWordScore(word, n) {
+function getWordScore(word, HANDSIZE) {
   let score = 0;
-  if (n === word.length) {
+  if (HANDSIZE === word.length) {
     score = 50;
   }
   for (let letter of word) {
-    score += SCRABBLE_LETTER_VALUES[letter] * word.length;
+    score += LETTER_VALUES[letter] * word.length;
   }
   return score
 }
@@ -38,16 +39,16 @@ function displayHand(hand) {
   return output;
 }
 
-function dealHand(n) {
+function dealHand(HANDSIZE) {
   let hand = {};
-  let numVowels = Math.floor(n / 3);
+  let numVowels = Math.floor(HANDSIZE / 3);
 
   for (let i = 0; i < numVowels; i++) {
     let item = VOWELS[Math.floor(Math.random() * VOWELS.length)];
     hand[item] ? hand[item]++ : hand[item] = 1;
   }
 
-  for (let i = 0; i < n - numVowels; i++) {
+  for (let i = 0; i < HANDSIZE - numVowels; i++) {
     let item = CONSONANTS[Math.floor(Math.random() * CONSONANTS.length)];
     hand[item] ? hand[item]++ : hand[item] = 1;
   }
@@ -70,9 +71,11 @@ function updateHand(hand, word) {
   return handCopy
 }
 
-function isValidWord(word, hand, WORDLIST) {
+function isValidWord(word, hand, WORDS) {
+  let letters = Object.keys(hand).sort();
   let handCopy = {...hand};
-  if (WORDLIST.includes(word.toUpperCase())) {
+  
+  if (letters.some(letter => WORDS[letter].includes(word))) {
     return word.split("").every(letter => {
       handCopy[letter]--;
       return handCopy[letter] >= 0;
@@ -82,13 +85,21 @@ function isValidWord(word, hand, WORDLIST) {
   }
 };
 
+function compSearch(word, HAND) {
+  let handCopy = {...HAND};
+  return word.split("").every(letter => {
+    handCopy[letter]--;
+    return handCopy[letter] >= 0;
+  })
+};
 
 function calculateHandlen(hand) {
   return Object.entries(hand).reduce((a, item) => item[1] + a, 0);
 }
 
 
-function playHand(hand, WORDLIST, n) {
+function playHand(hand, WORDS, HANDSIZE) {
+  
   let total = 0;
   
   DISPLAYHAND.innerHTML = displayHand(hand);
@@ -101,11 +112,11 @@ function playHand(hand, WORDLIST, n) {
       DIALOGUE.innerHTML += `\nGoodbye! Total score: ${total} points.`;
       return
     } else {
-      if (isValidWord(word, hand, WORDLIST) === false) {
+      if (isValidWord(word, hand, WORDS) === false) {
         DIALOGUE.innerHTML += "\nInvalid word, please try again.";
       } else {
-        total += getWordScore(word, n);
-        DIALOGUE.innerHTML += `\n"${word}" earned ${getWordScore(word, n)} points. Total: ${total} points`;
+        total += getWordScore(word, HANDSIZE);
+        DIALOGUE.innerHTML += `\n"${word}" earned ${getWordScore(word, HANDSIZE)} points. Total: ${total} points`;
         hand = updateHand(hand, word);
         DISPLAYHAND.innerHTML = "\n" + displayHand(hand);
       }
@@ -117,63 +128,85 @@ function playHand(hand, WORDLIST, n) {
   })
 }
 
-function compChooseWord(hand, WORDLIST, n) {
-  let bestScore = 0;
+function compChooseWord(hand) {
+  let letters = Object.keys(hand).sort();
   let bestWord = "";
-  for (let word of WORDLIST) {
-    if (isValidWord(word, hand, WORDLIST)) {
-      let score = getWordScore(word, n);
-      if (score > bestScore) {
-        bestScore = score;
-        bestWord = word;
+  let bestScore = 0;
+  for (let letter of letters) {
+    WORDS[letter].forEach(word => {
+      if (compSearch(word, hand) && getWordScore(word) > bestScore) {
+      bestScore = getWordScore(word)
+      bestWord = word;
       }
-    }
+    }); 
   }
   return bestWord
 }
 
-function compPlayHand(hand, WORDLIST, n) {
+function compPlayHand(hand, WORDS, HANDSIZE) {
   let totalScore = 0;
-  while (calculateHandLen(hand) > 0) {
+  let handLen = calculateHandlen(hand);
+  
+  while (handLen > 7) {
     DISPLAYHAND.innerHTML = displayHand(hand);
-    let word = compChooseWord(hand, WORDLIST, n);
+    let word = compChooseWord(hand, WORDS, HANDSIZE);
+    DIALOGUE.innerHTML += `COMPUTER FOUND "${word}"`;
     if (!word) {
+      DIALOGUE.innerHTML += "No word";
       break;
     } else {
-      if (!isValidWord(word, hand, WORDLIST)) {
+      if (!isValidWord(word, hand, WORDS)) {
         DISPLAYHAND.innerHTML = 'This is a terrible error! I need to check my own code!';
         break;
       } else {
-        let score = getWordScore(word, n);
+        let score = getWordScore(word, HANDSIZE);
         totalScore += score
-        DIALOGUE.innerHTML += `"${word}" earned ${getWordScore(word, n)} points. Total: ${total} points`;
+        DIALOGUE.innerHTML += `"${word}" earned ${getWordScore(word, HANDSIZE)} points. Total: ${totalScore} points`;
         hand = updateHand(hand, word);
       }
     }
   }
-  DIALOGUE.innerHTML += `Total score: ${total} points.`;
+  DIALOGUE.innerHTML += `Total score: ${totalScore} points.`;
 }
 
-function playGame(WORDLIST) {
+function playGame(WORDS) {
   let count = 0;
-  let hand = dealHand(HAND_SIZE);
+  let hand = dealHand(HANDSIZE);
   const controlButtons = document.querySelectorAll('.control-buttons');
   document.querySelector("#game-controls").innerHTML = "Enter n to deal a new hand, r to replay the last hand, or e to end game:";
+  let whoPlays = 'player';
+
+  COMPPLAY.addEventListener('change', async (event) => {
+    event.preventDefault();
+    await COMPPLAY.checked ? whoPlays = 'comp' : whoPlays = 'player';
+  })
   
+
   for (let button of controlButtons) {
     button.addEventListener('click', async (event) => {
       event.preventDefault();
-      const game = await button.value;
+      const game = await button.value + whoPlays;
+      
       switch(game) {
-        case 'n':
-          hand = dealHand(HAND_SIZE);
-          playHand(hand, WORDLIST, HAND_SIZE);
+        case 'nplayer':
+          hand = dealHand(HANDSIZE);
+          playHand(hand, WORDS, HANDSIZE);
           count++;
           break;
-        case 'r':
+        case 'rplayer':
           count === 0 ?
           DIALOGUE.innerHTML = "You have not played a hand yet. Please play a new hand first!" :
-          playHand(hand, WORDLIST, HAND_SIZE);
+          playHand(hand, WORDS, HANDSIZE);
+          break;
+        case 'ncomp':
+          hand = dealHand(HANDSIZE);
+          compPlayHand(hand, WORDS, HANDSIZE)
+          count++;
+          break;
+        case 'rcomp':
+          count === 0 ?
+          DIALOGUE.innerHTML = "Please play a new hand first!" :
+          compPlayHand(hand, WORDS, HANDSIZE);
           break;
         case 'e':
           return "END OF GAME";
@@ -186,4 +219,4 @@ function playGame(WORDLIST) {
   
 }
 
-playGame(WORDLIST);
+playGame(WORDS);
